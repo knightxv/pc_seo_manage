798be07e8ffb29a028357f8c3dc34bbc0bd15366
chenjianwei <chenjianwei@ddmh5.com>
2017-11-16 14:29:13 +0800

    给seo的版本完成

--- app/router.js
+++ app/router.js
@@ -2,6 +2,9 @@
 
 module.exports = app => {
   app.get('/', 'home.index');
+  app.get('/news', 'home.news');
+  app.get('/games', 'home.games');
+  app.get('/contact', 'home.contact');
   app.get('/api/getNewsDetail', 'home.getNews');
   app.get('/api/getGameDetail', 'home.getGameDetail');
   app.get('/api/getNewsList', 'home.getNewsList');
